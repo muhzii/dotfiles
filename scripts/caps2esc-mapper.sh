6fb44e90acf832e8e08ea6c09064bb6f3d997f76
muhzii <airomyst517@gmail.com>
2022-12-27 00:21:26 +0200

    Few fixes

--- scripts/caps2esc-mapper.sh
+++ scripts/caps2esc-mapper.sh
@@ -4,5 +4,5 @@ evdev=$1
 
 is_kbd=$(ls -al /dev/input/by-path/ | grep $evdev | grep kbd)
 if [ ! -z "$is_kbd" ]; then
-    evscript -f /usr/local/share/caps2esc_mapper.dyon -d /dev/input/$evdev
+    evscript -f /usr/local/share/caps2esc-mapper.dyon -d /dev/input/$evdev
 fi
